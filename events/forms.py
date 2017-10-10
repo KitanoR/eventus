fe7c619e820c08d10e92b4481ce123847d742cc5
Caye <cayezapeta@yahoo.com>
2017-10-10 12:49:33 -0600

    Formulario

--- events/forms.py
+++ events/forms.py
@@ -4,7 +4,39 @@ from .models import Categoria, Evento ,Asistente, Comentario, Participante
 class EventoForm(forms.ModelForm):
     class Meta:
         model = Evento
-        fields = ('nombre', 'sumario','contenido','categoria','lugar','inicio','final','imagen','is_free','precio',)
+        fields = [
+            'nombre',
+            'sumario',
+            'contenido',
+            'categoria',
+            'lugar',
+            'inicio',
+            'final',
+            'imagen',
+            'is_free',
+            'precio',
+        ]
+        labels = {
+            'nombre': 'Nombre',
+            'sumario':'Sumario',
+            'contenido': 'Contenido',
+            'categoria': 'Categoria',
+            'lugar': 'Lugar',
+            'inicio': 'Inicio',
+            'final': 'Final',
+            'imagen': 'Imagen',
+            'is_free': 'Gratis',
+            'precio': 'Precio',
+        }
+        widgets = {
+            'nombre': forms.TextInput(attrs ={'class': 'form-control'}),
+            'sumario': forms.Textarea(attrs = {'class': 'form-control'}),
+            'contenido': forms.Textarea(attrs = {'class': 'form-control'}),
+            'categoria': forms.Select(attrs = {'class': 'form-control'}),
+            'lugar': forms.TextInput(attrs = {'class': 'form-control'}),
+            'inicio': forms.DateInput(format = ('%d-%m-%Y'), attrs = {'class': 'form-control','placeholder':'Seleccione una fecha'}),
+            'final': forms.DateInput(format = ('%d-%m-%Y'), attrs = {'class': 'form-control','placeholder':'Seleccione una fecha'}),
+        }
 class ComentarioForm(forms.ModelForm):
     class Meta:
         model = Comentario
